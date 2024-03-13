fcb0c1fe579de026133208f724c828d30024c49f
sunsetheus <williamsalimrjs@gmail.com>
2024-03-13 20:50:22 +0700

    fix optimize

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -3,10 +3,12 @@ package com.advpro.profiling.tutorial.repository;
 import com.advpro.profiling.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 /**
  * @author muhammad.khadafi
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findTopByOrderByGpaDesc();
 }
